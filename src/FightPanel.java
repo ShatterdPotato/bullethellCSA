26e3e20c7e032c6b4ecbef0db37a4260cbc567e6
Sameer Ahmed <saahmed5526@gmail.com>
2025-05-22 13:59:57 -0400

    Rudimentary movement added

--- src/FightPanel.java
+++ src/FightPanel.java
@@ -1,10 +1,65 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class FightPanel extends JPanel {
+public class FightPanel extends JPanel implements KeyListener, ActionListener {
     private Player player;
+    private boolean[] keys;
+    private Timer timer;
 
     public FightPanel() {
+        addKeyListener(this);
+        setFocusable(true);
+        requestFocusInWindow();
         player = new Player();
+        keys = new boolean[128];
+        timer = new Timer(16, this);
+        timer.start();
     }
 
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (keys[87]) {
+            player.moveUp();
+        }
+
+        if (keys[65]) {
+            player.moveLeft();
+        }
+
+        if (keys[83]) {
+            player.moveDown();
+        }
+
+        if (keys[68]) {
+            player.moveRight();
+        }
+        g.drawImage(player.getSprite(), player.getX(),player.getY(), null);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == timer) {
+            repaint();
+        }
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,25 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Player {
     private int hearts;
     private int X;
     private int Y;
     public boolean dead;
+    private BufferedImage sprite;
 
     public Player() {
         hearts = 3;
         dead = false;
         X = 400;
         Y = 300;
+        try {
+            sprite = ImageIO.read(new File("src\\player.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public int getHearts() {
@@ -19,11 +30,40 @@ public class Player {
         return dead;
     }
 
+    public BufferedImage getSprite() {
+        return sprite;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
     public void hit() {
         hearts--;
         if (hearts == 0) {
             dead = true;
         }
+    }
+
+    public void moveUp() {
+        Y--;
+    }
+
+    public void moveDown() {
+        Y++;
+    }
+
+
+    public void moveRight() {
+        X++;
+    }
+
+    public void moveLeft() {
+       X--;
     }
 
 
